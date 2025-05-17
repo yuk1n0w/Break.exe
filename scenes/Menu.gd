faa9b2d4c174011242daf22c8fe56a9b265e86b2
yuk1n0w <aaditkumar11031972@gmail.com>
2025-05-17 17:28:15 +0100

    Created Main Menu with few buttons, and a placeholder Main scene

---
+++ scenes/Menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
